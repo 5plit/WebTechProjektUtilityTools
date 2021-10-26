b0cef4c0c18088f8b6b1f4e56b1372a89a58c181
Hauke <hauke.nahrath@web.de>
2021-10-27 01:32:36 +0200

    init

---
+++ src/main/java/de/WebTechProjekt/UtilityTools/UtilityToolsApplication.java
@@ -0,0 +1,13 @@
+package de.WebTechProjekt.UtilityTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityToolsApplication.class, args);
+	}
+
+}
